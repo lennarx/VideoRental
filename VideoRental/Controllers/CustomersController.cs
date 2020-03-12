4b04c986c3d0364ddf9cd8fbff810203c51d8dd0
Franco Redoni <Franco.Redoni@ey.com>
2020-03-11 21:58:08 -0300

    setup complete

--- VideoRental/Controllers/CustomersController.cs
+++ VideoRental/Controllers/CustomersController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Vidly.Models;
+using VideoRental.Models;
 
 namespace Vidly.Controllers
 {
--- VideoRental/Controllers/MoviesController.cs
+++ VideoRental/Controllers/MoviesController.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Vidly.Models;
-using Vidly.ViewModels;
+using VideoRental.Models;
 
 namespace Vidly.Controllers
 {
